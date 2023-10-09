c6defcb9050756075a022467d5dbe5283ecd98c3
danilolaet <danilomirandadesenvolvedor@gmail.com>
2023-10-09 15:16:30 -0300

    Criação dos principais tipos de variáveis

--- modulo-java-fundamentos/MeuPrimeiroProg/src/ClassePrincipal.java
+++ modulo-java-fundamentos/MeuPrimeiroProg/src/ClassePrincipal.java
@@ -1,2 +1,10 @@
+import javax.sound.sampled.SourceDataLine;
+
 public class ClassePrincipal {
+
+    public static void main (String[]args) {
+        final float numero = 855.6f;
+
+        System.out.println("O valor do número é:" + numero);
+    }
 }
