4d47c3a298aa1504c2942bc5bdf6580afebb6f15
danilolaet <danilomirandadesenvolvedor@gmail.com>
2023-10-10 14:42:53 -0300

    Incrementação e decrementação

--- modulo-java-fundamentos/OperadoresAritmeticos/src/Aritmeticos.java
+++ modulo-java-fundamentos/OperadoresAritmeticos/src/Aritmeticos.java
@@ -1,3 +1,5 @@
+import java.lang.invoke.MethodHandles;
+
 public class Aritmeticos {
     public static void main(String[] args) {
        int num1,num2,num3;
@@ -24,5 +26,21 @@ public class Aritmeticos {
         num2 = 20;
         num3 = num1 / num2;
         System.out.println("O resultado da divisão é:" + num3);
+        System.out.println("---------------------------------------");
+
+        //incrementação
+        num1 = 1;
+        while (num1 <= 10){
+        System.out.println(num1);
+        num1++;
+        }
+
+        System.out.println("---------------------------------------");
+        //decrementação
+        num1 = 10;
+        while (num1 > 0) {
+            System.out.println(num1);
+            num1--;
+        }
     }
 }
