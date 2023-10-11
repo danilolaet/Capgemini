4fa03883adf8cb944e12d00182096f79567489e4
danilolaet <danilomirandadesenvolvedor@gmail.com>
2023-10-11 11:18:41 -0300

    Criação de método de uma classe

---
+++ modulo-java-fundamentos/ClasseConceito/src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    public String nomePessoa;
+    public int idadePessoa;
+
+    public void MostraDados (String pNome, int pIdade){
+        System.out.println("Nome: " + pNome);
+        System.out.println("Idade: " + pIdade);
+    }
+}
---
+++ modulo-java-fundamentos/ClasseConceito/src/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pessoa pes = new Pessoa();
+        pes.nomePessoa = "Danilo";
+        pes.idadePessoa = 26;
+        pes.MostraDados(pes.nomePessoa, pes.idadePessoa);
+    }
+}
