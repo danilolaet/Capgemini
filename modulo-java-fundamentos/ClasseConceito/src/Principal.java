10bda2e144e4c103251261cd3981a4e27ecb0250
danilolaet <danilomirandadesenvolvedor@gmail.com>
2023-10-11 15:08:53 -0300

    Encapsulamento

--- modulo-java-fundamentos/ClasseConceito/src/Principal.java
+++ modulo-java-fundamentos/ClasseConceito/src/Principal.java
@@ -1,8 +1,9 @@
 public class Principal {
     public static void main(String[] args) {
         Pessoa pes = new Pessoa();
-        pes.nomePessoa = "Danilo";
-        pes.idadePessoa = 26;
-        pes.MostraDados(pes.nomePessoa, pes.idadePessoa);
+        pes.setNomePessoa("Danilo");
+        pes.setIdadePessoa(26);
+        System.out.println(pes.toString());
+
     }
 }
