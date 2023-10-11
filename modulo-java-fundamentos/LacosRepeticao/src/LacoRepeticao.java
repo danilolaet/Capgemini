77151a1419ba9751a073f19da3a435cecad26444
danilolaet <danilomirandadesenvolvedor@gmail.com>
2023-10-11 10:27:28 -0300

    Laço de repetição while

---
+++ modulo-java-fundamentos/LacosRepeticao/src/LacoRepeticao.java
@@ -0,0 +1,12 @@
+public class LacoRepeticao {
+    public static void main(String[] args) {
+        int QuantVezes = 1;
+
+        while (QuantVezes <= 10) {
+            System.out.println("Estou fazendo a " + QuantVezes + "ª vez");
+            QuantVezes++;
+        }
+            System.out.println("Saí do laço e executei " + (QuantVezes -1) + " vezes");
+        }
+    }
+
