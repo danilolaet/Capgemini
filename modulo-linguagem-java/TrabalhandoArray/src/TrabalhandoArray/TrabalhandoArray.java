1ceb73e2504bc38ec6d6b28b838b1956b9da1b26
danilolaet <danilomirandadesenvolvedor@gmail.com>
2023-10-18 15:34:42 -0300

    Trabalhando em arrays

--- modulo-linguagem-java/TrabalhandoArray/src/TrabalhandoArray/TrabalhandoArray.java
+++ modulo-linguagem-java/TrabalhandoArray/src/TrabalhandoArray/TrabalhandoArray.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class TrabalhandoArray {
     public static void main(String[] args) {
         String [] paises = new String[4];
+        int [] numeros = new int[10];
 
         for (int i = 0; i < 4; i++) {
             paises[i] = JOptionPane.showInputDialog("Informe o país");
@@ -13,5 +14,21 @@ public class TrabalhandoArray {
         for (String listaPaises : paises) {
             System.out.println(listaPaises);
         }
+
+        for (int i = 0; i < 10; i++) {
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Infome um número"));
+        }
+
+        for (Integer listaNumeros : numeros) {
+            System.out.println(listaNumeros);
+        }
+
+        int novoValor = 11;
+        numeros[8]= novoValor + 4;
+
+        for (Integer listaNumeros : numeros) {
+            System.out.println(listaNumeros);
+        }
+
     }
 }
